2b477db46b55d1b5ca64eb695fb43adb3023a771
Nils Knappmeier <nils.knappmier@cosee.biz>
2019-11-11 14:43:28 +0100

    add config and palette

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'Lightning Talks 2019',
+    description: 'Just playing around',
+    themeConfig: {
+        logo: '/logo.svg',
+        sidebar: [
+            '/',
+            '/some-more.md',
+            '/folder/third-file.md'
+        ],
+        nav: [
+            { text: 'Google', link: 'https://google.com' }
+        ]
+    }
+}
